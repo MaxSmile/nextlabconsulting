2912899d8fc126b2597f8aafb6116a78db7b4c6d
Maxim Vasilkov <maxim.vasilkov@gmail.com>
2024-04-13 00:38:15 +0930

    temp

--- app/terms-of-service/page.jsx
+++ app/terms-of-service/page.jsx
@@ -4,56 +4,44 @@ import { useEffect } from "react";
 import ContactForm from "../_components/ContactFormSection";
 
 export default function TermsPage() {
- let webSiteULR = "nextlabconsulting.com";
- useEffect(() => {
-    webSiteULR = window.location.href;
- },[]);
+  let webSiteURL = "nextlabconsulting.com";
+  useEffect(() => {
+    webSiteURL = window.location.href;
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-
       <section className="container mx-auto max-w-4xl mt-40 px-4 py-8">
-        <h1 className="text-3xl font-semibold mb-4">Privacy Policy</h1>
-        <p className="mb-4">This Privacy Policy describes how NextLab Consulting ("we", "us", or "our") collects, uses, and shares your personal information when you use our website at <strong>{webSiteULR}</strong> (the "Site").</p>
+        <h1 className="text-3xl font-semibold mb-4">Terms of Service</h1>
+        <p className="mb-4">These Terms of Service govern your use of the website located at <strong>{webSiteURL}</strong> and any related services provided by NextLab Consulting.</p>
+        <p className="mb-4">By accessing <strong>{webSiteURL}</strong>, you agree to abide by these Terms of Service and any amendments to the terms. If you do not agree to obey these Terms of Service, you are not permitted to use or access the website.</p>
 
-        <h2 className="text-xl font-semibold mb-2">Information We Collect</h2>
-        <p className="mb-4">We use Google Analytics to collect and analyze information about how visitors interact with our website. Google Analytics collects various types of data, including:</p>
+        <h2 className="text-xl font-semibold mb-2">Use License</h2>
+        <p className="mb-4">Permission is granted to temporarily download one copy of the materials on NextLab Consulting's website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license, you may not:</p>
         <ul className="list-disc ml-8 mb-4">
-          <li>IP addresses</li>
-          <li>Browser types</li>
-          <li>Pages visited</li>
-          <li>Time spent on each page</li>
-          <li>Referring websites</li>
-          <li>Device type</li>
-          <li>Operating system</li>
+          <li>Modify or copy the materials;</li>
+          <li>Use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+          <li>Attempt to decompile or reverse engineer any software contained on NextLab Consulting's website;</li>
+          <li>Remove any copyright or other proprietary notations from the materials;</li>
+          <li>Transfer the materials to another person or "mirror" the materials on any other server.</li>
         </ul>
-        <p className="mb-4">Google Analytics does not collect any personally identifiable information (PII). We use this information to understand how visitors engage with our website and to improve our services and user experience.</p>
-        <p className="mb-4">For more information on how Google Analytics handles user data, please refer to Google's Privacy Policy: <a href="https://policies.google.com/privacy" className="text-blue-500 underline">Google Privacy Policy</a>.</p>
+        <p className="mb-4">This license shall automatically terminate if you violate any of these restrictions and may be terminated by NextLab Consulting at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.</p>
 
-        <p className="mb-4">If you choose to contact us through the contact form on our website, we may collect the following personal information:</p>
-        <ul className="list-disc ml-8 mb-4">
-          <li>Name</li>
-          <li>Email address</li>
-          <li>Phone number</li>
-          <li>Institution</li>
-          <li>Message content</li>
-        </ul>
-        <p className="mb-4">We use this information solely for the purpose of responding to your inquiries and providing assistance. We do not use the information submitted through the contact form for marketing purposes unless explicitly consented by the user.</p>
+        <h2 className="text-xl font-semibold mb-2">Disclaimer</h2>
+        <p className="mb-4">The materials on NextLab Consulting's website are provided on an 'as is' basis. NextLab Consulting makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+        <p className="mb-4">Further, NextLab Consulting does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its website or otherwise relating to such materials or on any sites linked to this site.</p>
 
-        <h2 className="text-xl font-semibold mb-2">Sharing of Information</h2>
-        <p className="mb-4">We do not sell, trade, or otherwise transfer your personal information to third parties unless we provide you with advance notice. This does not include website hosting partners and other parties who assist us in operating our website, conducting our business, or serving our users, as long as those parties agree to keep this information confidential.</p>
+        <h2 className="text-xl font-semibold mb-2">Limitations</h2>
+        <p className="mb-4">In no event shall NextLab Consulting or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on NextLab Consulting's website, even if NextLab Consulting or a NextLab Consulting authorized representative has been notified orally or in writing of the possibility of such damage.</p>
 
-        <h2 className="text-xl font-semibold mb-2">Data Security</h2>
-        <p className="mb-4">We implement a variety of security measures to maintain the safety of your personal information when you enter, submit, or access your personal information.</p>
-
-        <h2 className="text-xl font-semibold mb-2">Your Rights</h2>
-        <p className="mb-4">You have the right to request access to, correction of, deletion of, or restrictions on your personal data collected by us. You also have the right to withdraw your consent for processing your personal data at any time. To exercise these rights, please contact us using the information provided below.</p>
+        <h2 className="text-xl font-semibold mb-2">Governing Law</h2>
+        <p className="mb-4">These terms and conditions are governed by and construed in accordance with the laws of [Your State/Province] and you irrevocably submit to the exclusive jurisdiction of the courts in that State or location.</p>
 
         <h2 className="text-xl font-semibold mb-2">Contact Us</h2>
-        <p>If you have any questions about this Privacy Policy or our practices regarding your personal information, please contact us using the form below.</p>
-        
+        <p>If you have any questions about these Terms of Service, please contact us using the form below.</p>
+
       </section>
-      
+
       <ContactForm />
     </main>
   );
