7f048024ed31ecd76f90d91a6993409f8cdcd535
cooltesla <167200114+cooltesla@users.noreply.github.com>
2024-04-18 22:16:28 +0930

    Update ContactFormSection.jsx

--- app/_components/ContactFormSection.jsx
+++ app/_components/ContactFormSection.jsx
@@ -35,7 +35,7 @@ function ContactFormSection() {
                   and help us better understand your needs, ensuring we provide
                   the best possible service.</p>
                   <p className="mt-8">
-                  Email us for any question at <a href='mailto:info@nextlabconsultion.com' 
+                  Email us for any question at <a href='mailto:info@nextlabconsulting.com' 
                     className="text-blue-200 underline hover:opacity-80" target='_blank'>info@nextlabconsultion.com</a>
                     </p>
                 </div>
