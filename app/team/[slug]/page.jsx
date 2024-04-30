1033a6e0d2adc2edb0dfa73b72f6b506379b24d3
Harristocles <harrispano@gmail.com>
2024-05-01 09:34:19 +1000

    Google Analytics Team pages

--- app/team/[slug]/page.jsx
+++ app/team/[slug]/page.jsx
@@ -2,6 +2,7 @@
 
 import { getMemberBySlug } from "../../_components/DataManager";
 import { MemberHeader } from "../../_components/MemberHeader";
+import { GoogleAnalytics } from '@next/third-parties/google';
 import ArticleData from "../../_components/ArticleData";
 export default async function TeamPage({ params }) {
     const member = getMemberBySlug(params.slug);
@@ -12,12 +13,15 @@ export default async function TeamPage({ params }) {
     
     
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between">
-            <article className="container mx-auto max-w-4xl mt-40 px-4 py-8 text-gray-800 whitespace-pre-wrap">
-                <MemberHeader article={member} />
+        <>
+            <GoogleAnalytics gaId="G-ZQVHG5TKH9" />
+            <main className="flex min-h-screen flex-col items-center justify-between">
+                <article className="container mx-auto max-w-4xl mt-40 px-4 py-8 text-gray-800 whitespace-pre-wrap">
+                    <MemberHeader article={member} />
                
-                <ArticleData article={member.content} />
-            </article>
-        </main>
+                  <ArticleData article={member.content} />
+                </article>
+            </main>
+        </>    
     );
 }
